57db5b8ff72b3daede7dd778a505c74e7a134d78
valve01 <118441831+valve01@users.noreply.github.com>
2024-08-23 14:03:15 +0300

    Анимация отзывов, преимущества, новости, ховеры кнопок и пунктов меню. Изменил цвета выпадающего меню в шапке

--- src/js/modules/lottieAnim.js
+++ src/js/modules/lottieAnim.js
@@ -1,13 +1,15 @@
 import lottie from 'lottie-web';
 import { defineElement } from '@lordicon/element';
-// define "lord-icon" custom element with default properties
+// // define "lord-icon" custom element with default properties
 defineElement(lottie.loadAnimation);
+
+
 
 import { DotLottie } from '@lottiefiles/dotlottie-web';
 const lottieEl = document.querySelector('.dotlottie-canvas');
 let dotLottie = new DotLottie({
 	canvas: lottieEl,
-	src: './img/icons/userAnim.json', 
+	src: './img/icons/userAnim.json',
 });
 
 lottieEl.addEventListener('mouseover', () => {
@@ -18,7 +20,7 @@ const phoneContainer=document.querySelector('.footer__phone')
 const lottiePhoneEl = document.querySelector('.dotlottie-phone');
 let lottiePhoneAnim = new DotLottie({
 	canvas: lottiePhoneEl,
-	src: './img/icons/phone_3.json', 
+	src: './img/icons/phone_3.json',
 });
 
 phoneContainer.addEventListener('mouseover', () => {
@@ -30,7 +32,7 @@ const emailContainer =document.querySelector('.footer__email')
 const lottieMailEl = document.querySelector('.dotlottie-mail');
 let lottieMailAnim = new DotLottie({
 	canvas: lottieMailEl,
-	src: './img/icons/mail_3.json', 
+	src: './img/icons/mail_3.json',
 });
 
 emailContainer.addEventListener('mouseover', () => {
--- src/js/modules/reviewsAnim.js
+++ src/js/modules/reviewsAnim.js
@@ -1,14 +1,15 @@
-const reviewsItems = document.querySelector('.reviews__items');
-let { top } = reviewsItems.getBoundingClientRect();
-let clientHeightValue = window.scrollY ;
+const reviewsItems = document.querySelectorAll('.reviews__item');
+
+const reviewsItemsList = document.querySelector('.reviews__item');
+let { top } = reviewsItemsList.getBoundingClientRect();
+let clientHeightValue = window.scrollY;
 
 window.addEventListener('scroll', () => {
-	clientHeightValue = window.scrollY + 600;
-	console.log(top, clientHeightValue)
+	clientHeightValue = window.scrollY + 400;
+	console.log(top, clientHeightValue);
 	if (top < clientHeightValue) {
-		reviewsItems.classList.add('visible');
-		
+		reviewsItems.forEach((item) => {
+			item.classList.add('visible');
+		});
 	}
 });
-
-
