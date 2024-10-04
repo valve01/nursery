8e4875f4ebf053a91edea3363e2f06809be76aba
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-04 16:26:24 +0300

    d

--- src/js/modules/fancyCarousel.js
+++ src/js/modules/fancyCarousel.js
@@ -1,15 +1,16 @@
 import { Carousel } from '@fancyapps/ui/dist/carousel/carousel.esm.js';
 import '@fancyapps/ui/dist/carousel/carousel.css';
-// import { Autoplay } from "@fancyapps/ui/dist/carousel/carousel.autoplay.esm.js";
+import { Autoplay } from "@fancyapps/ui/dist/carousel/carousel.autoplay.esm.js";
+import "@fancyapps/ui/dist/carousel/carousel.autoplay.css";
 
 
 const container = document.getElementById('myCarousel');
 const options = {
 	infinite: true,
-	// Autoplay: {
-	// 	timeout: 3000,
-	// },
-	// slidesPerPage:6
+	Autoplay: {
+		timeout: 3000,
+	  },
+	  slidesPerPage:1
 };
 
-new Carousel(container, options);
+new Carousel(container, options,{ Autoplay });
