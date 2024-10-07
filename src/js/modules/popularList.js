5fdae0101d9453bfa1bd963c12e7ca7248161073
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-07 09:38:10 +0300

    Разобрался как пересобирать массив из нужных объектов. List.js

--- src/js/modules/popularList.js
+++ src/js/modules/popularList.js
@@ -2,8 +2,8 @@ import List from 'list.js';
 
 var monkeyList = new List('test-list', {
 	valueNames: ['name'],
-	// page: 12,
-	// pagination: true,
+	page: 12,
+	pagination: true,
 });
 
 const funcPrevent = () => {
@@ -26,13 +26,24 @@ monkeyList.on('updated', () => {
 	}, 0);
 });
 
-// const categuryLinksForList = document.querySelectorAll('.popular__categories-link');
-// categuryLinksForList.forEach((link) => {
-// 	link.addEventListener('click', () => {
-// 		console.log(link, 'вот');
-// 		setTimeout(() => {
-// 			monkeyList.update();
-// 		}, 1000);
-		
-// 	});
-// });
+const categuryLinksForList = document.querySelectorAll('.popular__categories-link');
+categuryLinksForList.forEach((link) => {
+	link.addEventListener('click', () => {
+		console.log(link, 'вот');
+		setTimeout(() => {
+			monkeyList.clear()
+			// monkeyList.update();
+			monkeyList.add([{name:'Туя восточная Мадуродам'},{name:'Ель обыкновенная Уиллс Зверг'}])
+			// monkeyList.items.forEach((item, i) => {
+			// 	if (i == 1) {
+			// 		item.hide();
+			// 	}
+			// });
+			// monkeyList.update();
+			// monkeyList.items.forEach((item) => {
+			// 	item.show();
+			// });
+			// monkeyList.update();
+		}, 1000);
+	});
+});
