ca5cbb336e189445023da93aa0a6d9888463c668
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-07 15:13:31 +0300

    Реализовал табы через list js filter() . Начинаю переименовывать классы

--- src/js/modules/popularList.js
+++ src/js/modules/popularList.js
@@ -1,26 +1,23 @@
 import List from 'list.js';
 
-var monkeyList = new List('test-list', {
-	valueNames: ['name','category'],
+var cardsList = new List('list-container', {
+	valueNames: ['name', 'category'],
 	page: 12,
 	pagination: true,
 });
 
 const funcPrevent = () => {
 	const pages = document.querySelectorAll('.page');
-
 	for (let i = 0; i < pages.length; i++) {
 		const element = pages[i];
-
 		element.addEventListener('click', (e) => {
-		
 			e.preventDefault();
 		});
 	}
 };
 funcPrevent();
 
-monkeyList.on('updated', () => {
+cardsList.on('updated', () => {
 	setTimeout(() => {
 		funcPrevent();
 	}, 0);
@@ -28,37 +25,15 @@ monkeyList.on('updated', () => {
 
 const categuryLinksForList = document.querySelectorAll('.popular__categories-link');
 categuryLinksForList.forEach((link) => {
-	link.addEventListener('click', () => {
-	
-
-		monkeyList.filter(function (item) {
-			console.log(item)
-			if (item.values().category == 'Плодовые') {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		const currentCat = e.target.dataset['cat'];
+		cardsList.filter(function (item) {
+			if (item.values().category == currentCat || currentCat == 'all') {
 				return true;
 			} else {
 				return false;
 			}
 		});
-
-		// setTimeout(() => {
-		// monkeyList.clear();
-		// monkeyList.update();
-		// monkeyList.remove('name', 'Слива Медовая');
-		// monkeyList.update();
-		// monkeyList.items.forEach((item, i) => {
-		// 	if (i == 1) {
-		// 		item.hide();
-		// 	}
-		// });
-		// monkeyList.update();
-		// monkeyList.items.forEach((item) => {
-		// 	item.show();
-		// });
-		// monkeyList.update();
-		// }, 1000);
-		// setTimeout(() => {
-		// 	monkeyList.add('name', 'Слива Медовая');
-		// 	monkeyList.update();
-		// }, 5000);
 	});
 });
--- src/js/modules/popularTab.js
+++ src/js/modules/popularTab.js
@@ -1,19 +1,3 @@
-const popularCategoriesBtns = document.querySelectorAll('.popular__categories-link');
-const popularCats = document.querySelectorAll('.popular__item');
-
-popularCategoriesBtns.forEach((btn) => {
-	btn.addEventListener('click', (e) => {
-		// const currentCat = e.target.dataset['cat'];
-		// popularCats.forEach((cat) => {
-		// 	cat.classList.remove('none');
-		// 	if (cat.dataset['cat'] !== currentCat && currentCat !== 'all') {
-		// 		cat.classList.add('none');
-		// 	}
-		// });
-		e.preventDefault();
-	});
-});
-
 const btnsBuy = document.querySelectorAll('.popular__item-btn-buy');
 const btnsInCart = document.querySelectorAll('.popular__item-btn-in-cart');
 
