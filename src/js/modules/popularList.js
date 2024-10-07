97ecd5eb1d5f79e43c9d42d9756aab9b4df36b07
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-07 13:31:41 +0300

    Разобрался как фильтровать списки на List.js

--- src/js/modules/popularList.js
+++ src/js/modules/popularList.js
@@ -1,19 +1,19 @@
 import List from 'list.js';
 
 var monkeyList = new List('test-list', {
-	valueNames: ['name'],
+	valueNames: ['name','category'],
 	page: 12,
 	pagination: true,
 });
 
 const funcPrevent = () => {
 	const pages = document.querySelectorAll('.page');
-	console.log(pages);
+
 	for (let i = 0; i < pages.length; i++) {
 		const element = pages[i];
-		console.log(element);
+
 		element.addEventListener('click', (e) => {
-			console.log('wtf');
+		
 			e.preventDefault();
 		});
 	}
@@ -29,21 +29,36 @@ monkeyList.on('updated', () => {
 const categuryLinksForList = document.querySelectorAll('.popular__categories-link');
 categuryLinksForList.forEach((link) => {
 	link.addEventListener('click', () => {
-		console.log(link, 'вот');
-		setTimeout(() => {
-			monkeyList.clear()
-			// monkeyList.update();
-			monkeyList.add([{name:'Туя восточная Мадуродам'},{name:'Ель обыкновенная Уиллс Зверг'}])
-			// monkeyList.items.forEach((item, i) => {
-			// 	if (i == 1) {
-			// 		item.hide();
-			// 	}
-			// });
-			// monkeyList.update();
-			// monkeyList.items.forEach((item) => {
-			// 	item.show();
-			// });
-			// monkeyList.update();
-		}, 1000);
+	
+
+		monkeyList.filter(function (item) {
+			console.log(item)
+			if (item.values().category == 'Плодовые') {
+				return true;
+			} else {
+				return false;
+			}
+		});
+
+		// setTimeout(() => {
+		// monkeyList.clear();
+		// monkeyList.update();
+		// monkeyList.remove('name', 'Слива Медовая');
+		// monkeyList.update();
+		// monkeyList.items.forEach((item, i) => {
+		// 	if (i == 1) {
+		// 		item.hide();
+		// 	}
+		// });
+		// monkeyList.update();
+		// monkeyList.items.forEach((item) => {
+		// 	item.show();
+		// });
+		// monkeyList.update();
+		// }, 1000);
+		// setTimeout(() => {
+		// 	monkeyList.add('name', 'Слива Медовая');
+		// 	monkeyList.update();
+		// }, 5000);
 	});
 });
--- src/js/modules/popularTab.js
+++ src/js/modules/popularTab.js
@@ -3,13 +3,13 @@ const popularCats = document.querySelectorAll('.popular__item');
 
 popularCategoriesBtns.forEach((btn) => {
 	btn.addEventListener('click', (e) => {
-		const currentCat = e.target.dataset['cat'];
-		popularCats.forEach((cat) => {
-			cat.classList.remove('none');
-			if (cat.dataset['cat'] !== currentCat && currentCat !== 'all') {
-				cat.classList.add('none');
-			}
-		});
+		// const currentCat = e.target.dataset['cat'];
+		// popularCats.forEach((cat) => {
+		// 	cat.classList.remove('none');
+		// 	if (cat.dataset['cat'] !== currentCat && currentCat !== 'all') {
+		// 		cat.classList.add('none');
+		// 	}
+		// });
 		e.preventDefault();
 	});
 });
