0ea6c6cd5c54d56973362d91e338c93ece677e2b
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-09 13:04:38 +0300

    Каталог готов без адаптации

--- src/js/modules/catalogList.js
+++ src/js/modules/catalogList.js
@@ -41,3 +41,10 @@ categuryLinksForList.forEach((link) => {
 		});
 	});
 });
+
+const clearInputBtn = document.querySelector('.search-and-pagination-wrapper .cross-search');
+const catalogInputSearch = document.querySelector('.search-and-pagination-wrapper .search');
+clearInputBtn.addEventListener('click', () => {
+	catalogInputSearch.value = '';
+	cardsList.search();
+});
