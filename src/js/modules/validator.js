eed4235b1c427fcd90833f01ec27e3640cc5c422
valve01 <118441831+valve01@users.noreply.github.com>
2024-10-22 10:28:47 +0300

    Чекбокс сделал

--- src/js/modules/validator.js
+++ src/js/modules/validator.js
@@ -1,11 +1,9 @@
 import { isEmail } from 'validator';
 import showPopup from './sweetAlarm';
-// import hideForm from './hideForm';
 
 const mailInput = document.querySelector('.email__input');
 const submitBtn = document.querySelector('.email__btn');
-const checkBox=document.querySelector('.email__checkbox-descr')
-
+const checkBox = document.querySelector('.email__checkbox');
 
 submitBtn.addEventListener('click', (e) => {
 	e.preventDefault();
@@ -20,13 +18,21 @@ submitBtn.addEventListener('click', (e) => {
 	}
 });
 
-mailInput.addEventListener('input', () => {
-	mailInput.setAttribute('placeholder', 'Ваш электронный адрес');
-	if (isEmail(mailInput.value)) {
+const inputHandler = () => {
+	if (isEmail(mailInput.value) && checkBox.checked) {
 		submitBtn.removeAttribute('disabled');
 		mailInput.classList.add('valid-value');
 	} else {
 		submitBtn.setAttribute('disabled', 'true');
 		mailInput.classList.remove('valid-value');
 	}
+};
+
+mailInput.addEventListener('input', () => {
+	mailInput.setAttribute('placeholder', 'Ваш электронный адрес');
+	inputHandler();
+});
+
+checkBox.addEventListener('input', () => {
+	inputHandler();
 });
